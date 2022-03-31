557a4e16fdd5153233868b3040435b421d9bdd63
pulsnar <50366211+pulsnar@users.noreply.github.com>
2022-03-31 21:11:44 +0900

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+console.log("Hello world!");
+
+const appear = document.querySelector('.appear'); 
+const cb = function(entries){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('inview');
+    }else{
+      entry.target.classList.remove('inview');
+    }
+  });
+}
+const io = new IntersectionObserver(cb);
+io.observe(appear);
